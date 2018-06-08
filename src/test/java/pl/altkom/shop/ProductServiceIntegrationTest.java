4502ab743c808e8394145651dd946f5b88bd3039
MirekSz <m.szajowski@gmail.com>
2018-06-08 11:43:03 +0200

    conditions

--- src/test/java/pl/altkom/shop/ProductServiceIntegrationTest.java
+++ src/test/java/pl/altkom/shop/ProductServiceIntegrationTest.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
@@ -29,6 +30,7 @@ public class ProductServiceIntegrationTest {
 	ProductRepo repo;
 
 	@Test
+	@IfProfileValue(name = "os.name", value = "Windows 7")
 	public void shouldAddProduct() throws Exception {
 		// given
 		Product product = new Product("SSD", "Szybki", 10, BigDecimal.TEN);
