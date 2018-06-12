e2c6a7efb6965ae692abe529c1f3e84421948b36
MirekSz <miro1994>
2018-06-12 17:57:31 +0200

    tests

---
+++ src/main/java/pl/altkom/shop/repo/SaleDocumentRepo.java
@@ -0,0 +1,10 @@
+package pl.altkom.shop.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import pl.altkom.shop.model.SaleDocument;
+
+@Repository
+public interface SaleDocumentRepo extends JpaRepository<SaleDocument, Long> {
+}
