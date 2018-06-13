3ef17b65a1665f988a56f42f529ec94d90108475
MirekSz <miro1994>
2018-06-13 17:47:14 +0200

    auditor

--- src/main/java/pl/altkom/shop/model/Product.java
+++ src/main/java/pl/altkom/shop/model/Product.java
@@ -1,16 +1,25 @@
 package pl.altkom.shop.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -22,6 +31,22 @@ public class Product {
 	private Integer quantity;
 	@NotNull
 	private BigDecimal price;
+
+	@Column(name = "created_date", nullable = false, updatable = false)
+	@CreatedDate
+	private LocalDateTime createdDate;
+
+	@Column(name = "modified_date")
+	@LastModifiedDate
+	private LocalDateTime modifiedDate;
+
+	@Column(name = "created_by")
+	@CreatedBy
+	private String createdBy;
+
+	@Column(name = "modified_by")
+	@LastModifiedBy
+	private String modifiedBy;
 
 	public Product(String name, String description, int quentity, BigDecimal price) {
 		this.name = name;
@@ -72,6 +97,38 @@ public class Product {
 
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(LocalDateTime modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
 	}
 
 }
