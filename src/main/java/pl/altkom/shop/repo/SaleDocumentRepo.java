31314576073e80c6da36fa68655de933c7235fce
MirekSz <miro1994>
2018-06-14 21:01:25 +0200

    queries

--- src/main/java/pl/altkom/shop/repo/SaleDocumentRepo.java
+++ src/main/java/pl/altkom/shop/repo/SaleDocumentRepo.java
@@ -1,6 +1,9 @@
 package pl.altkom.shop.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import pl.altkom.shop.model.SaleDocument;
@@ -9,4 +12,11 @@ import pl.altkom.shop.model.SaleDocument;
 public interface SaleDocumentRepo extends JpaRepository<SaleDocument, Long> {
 
 	SaleDocument findByNumber(String number);
+
+	List<SaleDocument> queryByOrderByTotalPrice();
+
+	List<SaleDocument> findTop10ByOrderById();
+
+	@Query("FROM SaleDocument where size(items) > 3")
+	List<SaleDocument> findWith3Items();
 }
